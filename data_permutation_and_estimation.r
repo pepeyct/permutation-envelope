20936b5ba68670273abce3c7c29b83fabe20cf2d
Chong <zhaochongyct@gmail.com>
2018-01-31 16:47:24 -0500

    envelope combine

--- data_permutation_and_estimation.r
+++ data_permutation_and_estimation.r
@@ -13,6 +13,8 @@ process_len = sapply(process, length)
 events_process = unlist(process)
 # generate 50 simulation permuation for all data points 
 TT=30
+rho=1.0
+sigma=0.025
 days=1:TT
 vec=process[[1]]
 N=100 # how many permutaion data need to be generated
@@ -157,9 +159,11 @@ h.optim
 #out = list(h.optim,MISE.h,estResult)
 save(estResult,file=paste0(workpath,"/local constant/local constant estimator optimal result with bandwidth h=",h.optim,".RData"))
 ############################################################
+file=paste0(workpath,"/local constant/local constant estimator optimal result with bandwidth h=",h.optim,".RData")
+load(file)
+
 for (sim in 1:100){
-  file=paste0(workpath,"/local constant/local constant estimator optimal result with bandwidth h=",h.optim,".RData")
-  load(file)
+
   n.grid= 500
   grid=seq(0,R,length.out = n.grid)
   at=estResult[,sim]
@@ -168,4 +172,81 @@ for (sim in 1:100){
     lines(grid,rep(1,500))}
   else { lines(grid,at,type="l",ylim=c(-1,16))}
 }
-lines(grid,pcfGrid,col="red",type="l") 
+lines(grid,pcfGrid,col="red",type="l")
+##################################################################
+
+
+load("C:/Users/CZhao/Dropbox/Research/PairCorrelationFunction/simulation/SimulationFinal/simulation with selection/setting1/rho1_sigma0.025/rho=1.0/data/Thomas data1.Rdata")
+
+ptm<-proc.time()
+#TT=30
+R.max = R+2*h.max
+#################################################################################
+#process=process_permu
+N  =length(process)
+ptm<-proc.time()
+event_num_N = sapply(process,length)
+event_process = unlist(process)
+# 
+l_event_process = length(event_process)
+dp=list(l_event_process)
+sp=list(l_event_process)
+loc_all = 1:l_event_process
+for(i in 1:N){
+  N_i = event_num_N[i]
+  sum_N_i = sum(event_num_N[1:i])
+  start= sum_N_i-N_i+1
+  loc_N_i =start:sum_N_i
+  for(j in loc_N_i){
+    start_point = event_process[j]
+    end_loc_sp = loc_N_i[loc_N_i!=j]
+    events_ij_sp = abs(start_point-event_process[ end_loc_sp])
+    retain_logi_sp= events_ij_sp<=R.max
+    dist_retain_sp=events_ij_sp[retain_logi_sp]
+    sp[[j]]=dist_retain_sp
+    # different process distance
+    events_ij_dp = abs(start_point-event_process[-(start:sum_N_i)])
+    retain_logi_dp= events_ij_dp<=R.max
+    dist_retain_dp=events_ij_dp[retain_logi_dp] 
+    dp[[j]]=dist_retain_dp
+  }
+}
+distVS=sort(unlist(sp))
+distVS=distVS[duplicated(distVS)]
+distVD=sort(unlist(dp))
+distVD=distVD[duplicated(distVD)]
+
+### calculate the distance vectors for points inside the Same process 
+# generate t sequence which represent the whole day 
+n.grid= 500
+grid=seq(0,R,length.out = n.grid)
+estResult = matrix(rep(0,n.grid*n.h),nrow = n.h) 
+for(n in 1:n.h){
+  bh=h.list[n]
+  # define a function for solve estimating equations
+  start_point1 = sapply(grid-bh,function(x) which.max(distVS >= x))
+  end_point1   = sapply(grid+bh,function(x) which.min(distVS <= x)-1)
+  start_point2 = sapply(grid-bh,function(x) which.max(distVD >= x))
+  end_point2   = sapply(grid+bh,function(x) which.min(distVD <= x)-1)
+  #############################################################
+  for (i in 1:n.grid){
+    RetainPair.1=distVS[start_point1[i]:end_point1[i]]
+    # retain pairs between different process
+    RetainPair.2=distVD[start_point2[i]:end_point2[i]]
+    ##############
+    A=length(RetainPair.1)*(N-1)
+    B=length(RetainPair.2)
+    estResult[n,i]=A/B
+  }
+}
+estResult
+proc.time()-ptm
+save(estResult,file=paste0(workpath,"/local constant/local constant permutation result.RData"))
+
+lines(grid,estResult[4,],col="blue",type="l",lty=2) 
+#########
+load("C:/Users/CZhao/Dropbox/Research/PairCorrelationFunction/data application/wb_application/topics/pemutation envelope/local constant/local constant estimator result.RData")
+estResult = Simu.array[4,,]
+var_permu = apply(estResult,1,var)
+
+
